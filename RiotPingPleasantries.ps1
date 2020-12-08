d5cabb76cf32434635d23cfe8477764838b9a7c0
metshcam <73416650+metshcam@users.noreply.github.com>
2020-12-08 10:05:45 -0500

    Update RiotPingPleasantries.ps1

--- RiotPingPleasantries.ps1
+++ RiotPingPleasantries.ps1
@@ -4,10 +4,17 @@ $ipaddr = '192.168.1.1'
 ## Once the IP stops pinging, run actions; in this case
 ## Warn riot that we are going to restart WLAN for her
 ## because the wifi card is problematic
+##
+
+## Start loop
 ## CTRL+C to break
 ##
 while ($true) {
+    ## Ping IP and if it responds, start the do loop
+    ##
     if (Test-NetConnection $ipaddr | Where-Object { $_.PingSucceeded }) {
+        ## Keep running the ping against the host until it doesn't
+        ##
         do {
             Write-Host "Pinging interwebz on $ipaddr ..."
             Start-Sleep -Seconds 3
@@ -22,7 +29,8 @@ while ($true) {
         Start-Process "netsh.exe"  -ArgumentList 'interface set interface "WLAN" ENABLED' -Wait
         Write-Host "Hey riot, something up with your waifu card!"
     }
-
+    ## If the IP address does not ping
+    ##
     elseif (Test-NetConnection $ipaddr | Where-Object { !($_.PingSucceeded) }) {
         Write-Host "I'm sorry riot, I can't seem to ping that host."
         }
